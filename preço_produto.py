498751292f25cdd840a0ad9d928e3940f9605278
Rogersamacedo <41793312+Rogersamacedo@users.noreply.github.com>
2021-09-13 12:30:29 -0300

    Add files via upload

---
+++ preço_produto.py
@@ -0,0 +1,14 @@
+def preço_produto():
+    n1 = input("Digite nome do produto: ")
+    n2 = float(input("DIgite preço do produto: "))
+    n3 = float(input("digite  quantidade e unidade do produto:  "))
+    ##n1 = n1
+    ##n2 = n2
+    ##n3 = n3
+        #valor = valor
+    valor =  n2 / n3
+   #preço_produto()
+    print("o preço cheio do(a)", n1, "é: ", valor)
+    
+
+preço_produto()
